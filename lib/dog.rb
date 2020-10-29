e157194c4e9de100fb0fb7aeb341aae14291a50c
Diana Mora <morra.dianna@gmail.com>
2020-10-29 17:02:59 -0400

    commit tree a658f7453039868deb8d605ea08addc108b66690

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+    def initialize(name, breed = 'Mutt')
+        @name = name
+        @breed = breed
+        # breed = "Mutt"
+    end
+    # def breed=(breed = nil)
+    #     @name = breed
+    # end
+
+    # def breed
+    #     breed = "Mutt"
+    # end 
+  end 
